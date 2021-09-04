3b3193d81530ccad79b1f75891244121071176e1
Endre <tamas.nagy.endre@gmail.com>
2021-09-04 16:58:22 +0200

    REFACTOR: extract TODO into component

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,26 @@
 <template>
-<h1>TODO app</h1>
-<div class="todo-card">
-  <input  type="text" v-model="inputValue" />
-  <button @click="addToDo">Add</button>
-  <ul id="todo-list" v-for="todo in todos" :key="todo">
-    <li>{{todo}}</li>
-    <button @click="removeTodo">X</button>
-  </ul>
-</div>
+  <ToDo/>
+
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
 import HelloWorld from "./components/HelloWorld.vue";
+import ToDo from "./components/ToDo.vue"
 
 @Options({
   components: {
     HelloWorld,
+    ToDo
   },
 })
 export default class App extends Vue {
-  inputValue = "Note ToDo here"
-  todos: string[] = ["Be awesome", "Work out"]
 
-  public addToDo (){
-    if (this.inputValue){
-      //this.$emit("add-todo", this.inputValue)
-
-      console.log(`˙Adding ${this.inputValue}`)
-      this.todos.push(this.inputValue)
-      this.inputValue = ""
-    }
-  }
-  public removeTodo(){
-    console.log(`Remove `)
-  }
 }
 </script>
 
 <style>
-#todo-list{
-  border: #2c3e50 solid;
-  width: auto;
-  text-align: center;
-}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/ToDo.vue
@@ -0,0 +1,40 @@
+<template>
+  <h1>TODO app</h1>
+  <div class="todo-card">
+    <input  type="text" v-model="inputValue" />
+    <button @click="addToDo">Add</button>
+    <ul id="todo-list" v-for="todo in todos" :key="todo">
+      <li>{{todo}}</li>
+      <button @click="removeTodo">X</button>
+    </ul>
+  </div>
+</template>
+<script lang="ts">
+import { Vue } from "vue-class-component";
+
+export default class ToDo extends Vue {
+  inputValue = "Note ToDo here"
+  todos: string[] = ["Be awesome", "Work out"]
+
+  public addToDo (){
+    if (this.inputValue){
+      //this.$emit("add-todo", this.inputValue)
+
+      console.log(`˙Adding ${this.inputValue}`)
+      this.todos.push(this.inputValue)
+      this.inputValue = ""
+    }
+  }
+  public removeTodo(){
+    console.log(`Remove `)
+  }
+}
+</script>
+
+<style scoped>
+#todo-list{
+  border: #2c3e50 solid;
+  width: auto;
+  text-align: center;
+}
+</style>
